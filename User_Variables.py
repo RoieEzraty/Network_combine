5b2f29698e866928c38403c5f53e6a9a0a41f801
SMR_Admin <hadar.strauss@mail.huji.ac.il>
2024-09-24 11:37:34 +0300

    good params deltaR_propto_deltap

--- User_Variables.py
+++ User_Variables.py
@@ -86,7 +86,7 @@ class User_Variables:
             # Load the Iris dataset
             iris = load_iris()
             # dataset, numerical_targets = shuffle(iris['data'], iris['target'], random_state=42)
-            dataset, numerical_targets = shuffle(iris['data'], iris['target'], random_state=30)
+            dataset, numerical_targets = shuffle(iris['data'], iris['target'], random_state=31)
             # dataset, numerical_targets = shuffle(iris['data'], iris['target'], random_state=14)
             # Min-Max Scale dataset to [0, 5]
             min_max_scaler = MinMaxScaler(feature_range=(0, 5))

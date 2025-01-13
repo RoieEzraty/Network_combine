9faaf32c00924bc9ccc0952c6c3676c4ab7b5c70
SMR_Admin <hadar.strauss@mail.huji.ac.il>
2025-01-13 13:30:36 +0200

    last one before new brach R_propto_Power

--- Network_State.py
+++ Network_State.py
@@ -44,8 +44,8 @@ class Network_State:
         # # outputs and extra outputs during dual problem in time
         # self.output_dual_in_t: List[NDArray[np.float_]] = [1. * np.ones(Variabs.Nout)]
         # self.extraOutput_dual_in_t: List[NDArray[np.float_]] = [1. * np.ones(Variabs.extraNout)]
-        self.input_dual_in_t: List[NDArray[np.float_]] = [2. * np.ones(Variabs.Nin)]
-        self.extraInput_dual_in_t: List[NDArray[np.float_]] = [2. * np.ones(Variabs.extraNin)]
+        self.input_dual_in_t: List[NDArray[np.float_]] = [1. * np.ones(Variabs.Nin)]
+        self.extraInput_dual_in_t: List[NDArray[np.float_]] = [1. * np.ones(Variabs.extraNin)]
         self.inter_dual_in_t: List[NDArray[np.float_]] = [np.random.random(Variabs.Ninter)]
         # outputs and extra outputs during dual problem in time
         self.output_dual_in_t: List[NDArray[np.float_]] = [0. * np.ones(Variabs.Nout)]
@@ -479,7 +479,7 @@ class Network_State:
             else:
                 self.loss = BigClass.Variabs.loss_fn(self.output, self.desired)
         self.loss_norm = self.loss/np.mean(np.abs(self.desired))
-        
+
         self.loss_in_t.append(self.loss)
         self.loss_norm_in_t.append(self.loss_norm)
 
